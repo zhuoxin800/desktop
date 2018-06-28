1d53d92bb2526d93206c5e7a55e3224fe59ea93b
Brendan Forster <github@brendanforster.com>
2018-06-28 15:40:17 -0300

    extract this to a type

--- app/src/lib/app-state.ts
+++ app/src/lib/app-state.ts
@@ -689,6 +689,12 @@ export enum CompareActionKind {
   Branch = 'Branch',
 }
 
+export interface ICompareToBranch {
+  readonly kind: CompareActionKind.Branch
+  readonly branch: Branch
+  readonly mode: ComparisonView.Ahead | ComparisonView.Behind
+}
+
 /**
  * An action to send to the application store to update the compare state
  */
@@ -696,8 +702,4 @@ export type CompareAction =
   | {
       readonly kind: CompareActionKind.History
     }
-  | {
-      readonly kind: CompareActionKind.Branch
-      readonly branch: Branch
-      readonly mode: ComparisonView.Ahead | ComparisonView.Behind
-    }
+  | ICompareToBranch
